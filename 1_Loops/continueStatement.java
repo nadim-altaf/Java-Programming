e97f7098f3160f1a90fe72e6cb4e527f77b40d52
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-01 15:29:27 +0530

    Added Loops : continue statement & dowhile

---
+++ 1_Loops/continueStatement.java
@@ -0,0 +1,11 @@
+public class continueStatement {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 20; i++) {
+      if (i == 12)
+        continue;
+      System.out.println(i);
+
+    }
+  }
+}
