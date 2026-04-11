4e517180e693d6894fafe4763954d8480aaa69c8
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-11 11:02:37 +0530

    Added annoymousObject program

---
+++ OOPs/annoymousObject.java
@@ -0,0 +1,15 @@
+class bus{
+  int a;
+  String name ;
+
+  public void b(){
+    System.out.println("annoumous object");
+  }
+}
+public class annoymousObject {
+
+  public static void main(String[] args) {
+    
+    new bus().b(); // annoymous object
+  }
+}
