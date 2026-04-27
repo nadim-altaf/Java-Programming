d29a9669b92af635df98c5e26a3077dbcd9a734b
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-27 15:09:10 +0530

    Added standardLibaryMethod prject

---
+++ Project2/standardLibaryMethod.java
@@ -0,0 +1,13 @@
+public class standardLibaryMethod {
+  public static void main(String[] args) {
+    System.out.println(Math.sqrt(24));
+    System.out.println(Math.sqrt(16));
+
+    System.out.println(Math.floor(5.3));
+    System.out.println(Math.floor(5.8));
+
+    System.out.println(Math.ceil(5.1));
+    System.out.println(Math.ceil(5.9));
+
+  }
+}
