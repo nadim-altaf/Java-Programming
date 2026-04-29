c11f754b099573c61f5c8226b64b1d731f5aa766
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-29 13:40:19 +0530

    Added new programs

---
+++ Project2/whether.java
@@ -0,0 +1,11 @@
+class whether {
+
+ String tool = "hammer";
+ String tool2 = "saw";
+}
+class Main{
+  public static void main(String[] args) {
+    // whether a = new whether();
+    // a.tool();
+  }
+}
