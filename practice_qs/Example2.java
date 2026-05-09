f5473ac9779a85c1d3642deef0808d00f41f8e36
nadim-altaf <nadimaltaf271@gmail.com>
2026-05-10 00:14:58 +0530

    Added example 2

---
+++ practice_qs/Example2.java
@@ -0,0 +1,14 @@
+class a {
+      a()
+{
+      System.out.println("code os class a");
+}
+        }
+class Example2{
+
+      public static void main(String args[])
+  {
+      a a1 = new a();
+  }
+ 
+}
