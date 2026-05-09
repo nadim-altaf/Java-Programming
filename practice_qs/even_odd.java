8e74fb5ce473ccba6e3c04698bbc9e891e54bcbf
nadim-altaf <nadimaltaf271@gmail.com>
2026-05-10 00:12:52 +0530

    Added file

---
+++ practice_qs/even_odd.java
@@ -0,0 +1,19 @@
+class even_odd{
+
+	void evenOdd(int y)
+	{
+	if(y%2==0)
+	{
+	System.out.println("Even No.");
+	}
+	else{
+	System.out.println("Odd No.");
+	}}}
+class Example3{
+	public static void main(String args[])
+	{
+	even_odd a1 = new even_odd();
+		a1.evenOdd(5);
+}
+}
+	
