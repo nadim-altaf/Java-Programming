7704e550e284a112272633500ed51e0befcffc78
nadim-altaf <nadimaltaf271@gmail.com>
2026-05-10 00:13:33 +0530

    Added practice program

---
+++ practice_qs/Example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Example
+{
+ public static void main(String[] args) {
+
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter The number :");
+   int num = sc.nextInt();
+  System.out.println("The given number is : "+num);
+ }
+}
